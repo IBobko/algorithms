4090d4f967946e1502dfc8dc4657daf684e76791
Igor Bobko <limit-speed@yandex.ru>
2018-09-06 17:25:09 +0300

    Added Proxy pattern.

---
+++ src/main/java/ru/company/patterns/proxy/ThirdPartyYoutubeLib.java
@@ -0,0 +1,9 @@
+package ru.company.patterns.proxy;
+
+import java.util.HashMap;
+
+public interface ThirdPartyYoutubeLib {
+    public HashMap<String, Video> popularVideos();
+
+    public Video getVideo(String videoId);
+}
